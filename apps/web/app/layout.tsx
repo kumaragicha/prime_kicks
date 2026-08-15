b4049d513c6562d43605afba9830f7041f011c62
kumaragicha <kumaragicha@gmail.com>
2026-08-15 13:22:25 +0530

    added ga4

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import GoogleAnalyticsWrapper from './GoogleAnalytics';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
@@ -10,9 +11,12 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  console.log('process.env.NEXT_PUBLIC_GA_ID', process.env.NEXT_PUBLIC_GA_ID);
   return (
     <html lang="en" className="scroll-smooth">
       <body className="m-0 bg-paper text-ink font-[Arial,Helvetica,sans-serif]">
+        <GoogleAnalyticsWrapper />
+
         <Providers>{children}</Providers>
       </body>
     </html>
